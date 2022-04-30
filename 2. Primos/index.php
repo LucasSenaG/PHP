e50c7efefac0a3be1d0839e314aa3b3611955049
Lucas <lukasgirundi@gmail.com>
2022-04-29 23:34:17 -0300

    Exibe apenas os numero primos dentro do intervalo indicado

---
+++ 2. Primos/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2. Números Primos</title>
+</head>
+<body>
+<form action="funcao.php" method="get">
+    <h1>Insira um número em cada campo, retornaremos os números primos entre eles: </h1>
+    <input type="number" name="n1" id="n1" min="1">
+    <input type="number" name="n2" id="n2" min="1">
+    <button type="submit">Calcular</button>
+</form>    
+
+</body>
+</html>
